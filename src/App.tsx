07be7dcb131bee9dccbca7922d70c365724a4ef1
OsirisJr16 <mintsanavalonajunior@example.com>
2025-06-18 10:43:42 +0300

    loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
+import React from "react";
 import { Navbar } from "./layout/NavBar";
+import LoadingScreen from "./components/LoadingScreen";
 import Footer from "./layout/Footer";
 import HelloSection from "./components/HelloSection";
 import SkillSections from "./components/SkillsSection";
@@ -7,6 +9,13 @@ import ProjectSections from "./components/ProjectSections";
 import "./App.css";
 
 function App() {
+  const [loading, setLoading] = React.useState(true);
+  const handleLoadingComplete = () => {
+    setLoading(false);
+  };
+  if (loading) {
+    return <LoadingScreen onComplete={handleLoadingComplete} />;
+  }
   return (
     <div className="bg-gradient-to-br from-slate-900 via-slate-800 to-slate-900 overflow-x-hidden">
       <Navbar />
